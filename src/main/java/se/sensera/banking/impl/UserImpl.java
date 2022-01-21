78be6b0d08449e4416f6c9161e3c15e81c138221
atenghamn <alex.tenghamn@gmail.com>
2022-01-21 10:36:25 +0100

    created user class

---
+++ src/main/java/se/sensera/banking/impl/UserImpl.java
@@ -0,0 +1,40 @@
+package se.sensera.banking.impl;
+
+import se.sensera.banking.User;
+
+public class UserImpl implements User {
+    @java.lang.Override
+    public String getId() {
+        return null;
+    }
+
+    @java.lang.Override
+    public String getName() {
+        return null;
+    }
+
+    @java.lang.Override
+    public void setName(String name) {
+
+    }
+
+    @java.lang.Override
+    public String getPersonalIdentificationNumber() {
+        return null;
+    }
+
+    @java.lang.Override
+    public void setPersonalIdentificationNumber(String pid) {
+
+    }
+
+    @java.lang.Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @java.lang.Override
+    public void setActive(boolean active) {
+
+    }
+}
