80d8f6a551748c9d795928aa13dd136688fa272b
atenghamn <alex.tenghamn@gmail.com>
2022-01-30 23:35:38 +0100

    Solved the tests in TranscationServiceTest

--- src/main/java/se/sensera/banking/impl/TransactionImpl.java
+++ src/main/java/se/sensera/banking/impl/TransactionImpl.java
@@ -1,34 +1,51 @@
 package se.sensera.banking.impl;
 
 import se.sensera.banking.Account;
+import se.sensera.banking.AccountsRepository;
 import se.sensera.banking.Transaction;
 import se.sensera.banking.User;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class TransactionImpl implements Transaction {
+
+    private String id;
+    private Date created;
+    private User user;
+    private Account account;
+    private double amount;
+
+    public TransactionImpl(Date created, User user, Account account, double amount){
+        this.id = UUID.randomUUID().toString();
+        this.created = created;
+        this.user = user;
+        this.account = account;
+        this.amount = amount;
+    }
+
     @Override
     public String getId() {
-        return null;
+        return id;
     }
 
     @Override
     public Date getCreated() {
-        return null;
+        return created;
     }
 
     @Override
     public User getUser() {
-        return null;
+        return user;
     }
 
     @Override
     public Account getAccount() {
-        return null;
+        return account;
     }
 
     @Override
     public double getAmount() {
-        return 0;
+        return amount;
     }
 }
--- src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
+++ src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
@@ -1,20 +1,71 @@
 package se.sensera.banking.impl;
 
-import se.sensera.banking.Transaction;
-import se.sensera.banking.TransactionService;
+import se.sensera.banking.*;
+import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
+import se.sensera.banking.exceptions.UseExceptionType;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class TransactionServiceImpl implements TransactionService {
+
+    UsersRepository usersRepository;
+    AccountsRepository accountsRepository;
+    TransactionsRepository transactionsRepository;
+
+    public TransactionServiceImpl(UsersRepository usersRepository, AccountsRepository accountsRepository, TransactionsRepository transactionsRepository) {
+        this.usersRepository = usersRepository;
+        this.accountsRepository = accountsRepository;
+        this.transactionsRepository = transactionsRepository;
+    }
+
     @Override
     public Transaction createTransaction(String created, String userId, String accountId, double amount) throws UseException {
+        User user = usersRepository.getEntityById(userId).orElseThrow();
+        Account account = accountsRepository.getEntityById(accountId).orElseThrow();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        System.out.println(account.getOwner().getId());
+        System.out.println(account.getOwner().getName());
+        System.out.println(userId);
+        System.out.println(user.getName());
+
+        double sum = transactionsRepository.all()
+                .filter(x -> Objects.equals(x.getAccount().getId(), accountId)).mapToDouble(Transaction::getAmount).sum();
+
+        if ((sum + amount) < 0){
+            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_FUNDED);
+        }
+
+        if (!Objects.equals(account.getOwner().getId(), userId) &&  account.getUserList().noneMatch(x -> Objects.equals(x.getId(), userId))){
+           throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_ALLOWED);
+        }
+
+        try {
+            Date date = dateFormatter.parse(created);
+            TransactionImpl transaction = new TransactionImpl(date, user, account, amount);
+            return transactionsRepository.save(transaction);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public double sum(String created, String userId, String accountId) throws UseException {
-        return 0;
+
+
+      double d =   transactionsRepository.all()
+                .filter(x -> Objects.equals(x.getUser().getId(), userId))
+                .filter(x -> Objects.equals(x.getAccount().getId(), accountId)).mapToDouble(Transaction::getAmount).sum();
+        System.out.println(d);
+
+        return d;
+
     }
 
     @Override
