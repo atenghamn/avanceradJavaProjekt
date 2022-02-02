b29161e4e00399cff391ab04fdac9e05322d290e
Andreaslundberg93 <92013984+Andreaslundberg93@users.noreply.github.com>
2022-02-02 14:50:41 +0100

    A.L, M.E, A.T, Z.D Test cleared

--- src/main/java/se/sensera/banking/impl/AccountServiceImpl.java
+++ src/main/java/se/sensera/banking/impl/AccountServiceImpl.java
@@ -4,10 +4,15 @@ import se.sensera.banking.*;
 import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
 import se.sensera.banking.exceptions.UseExceptionType;
+import se.sensera.banking.utils.ListUtils;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AccountServiceImpl implements AccountService {
@@ -24,19 +29,18 @@ public class AccountServiceImpl implements AccountService {
     public Account createAccount(String userId, String accountName) throws UseException {
         User andreas = usersRepository.getEntityById(userId)
                 .orElseThrow(() -> new UseException(Activity.CREATE_ACCOUNT, UseExceptionType.USER_NOT_FOUND));
-
+        verifyAccountNameUniqueness(andreas, accountName);
         AccountImpl account = new AccountImpl(accountName, andreas, true);
 
-        boolean notUnique = accountsRepository.all()
-                .filter(x -> Objects.equals(x.getOwner().getName(), andreas.getName()))
-                .anyMatch(x -> Objects.equals(x.getName(), account.getName()));
+        return accountsRepository.save(account);
+    }
 
-        if (notUnique) {
+    private void verifyAccountNameUniqueness(User andreas, String accountName) throws UseException {
+        if (accountsRepository.all()
+                .filter(x -> Objects.equals(x.getOwner().getName(), andreas.getName()))
+                .anyMatch(x -> Objects.equals(x.getName(), accountName))) {
             throw new UseException(Activity.CREATE_ACCOUNT, UseExceptionType.ACCOUNT_NAME_NOT_UNIQUE);
         }
-
-
-        return accountsRepository.save(account);
     }
 
     @Override
@@ -72,15 +76,17 @@ public class AccountServiceImpl implements AccountService {
         Account account = accountsRepository.getEntityById(accountId).orElseThrow(() -> new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_FOUND));
         User otherUser = usersRepository.getEntityById(userIdToBeAssigned).orElseThrow();
 
-        if (!account.isActive()){
+        if (!account.isActive()) {
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.ACCOUNT_NOT_ACTIVE);
-        } else if(Objects.equals(userId, userIdToBeAssigned)){
+        }
+        if (Objects.equals(userId, userIdToBeAssigned)) {
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.CANNOT_ADD_OWNER_AS_USER);
-        } else if(account.getUsers()
-                .anyMatch(x -> Objects.equals(x.getId(), userIdToBeAssigned))){
+        }
+        if (account.getUsers()
+                .anyMatch(x -> Objects.equals(x.getId(), userIdToBeAssigned))) {
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.USER_ALREADY_ASSIGNED_TO_THIS_ACCOUNT);
         }
-        else if(!Objects.equals(userId, account.getOwner().getId())){
+        if (!Objects.equals(userId, account.getOwner().getId())) {
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_OWNER);
         }
         account.addUser(otherUser);
@@ -91,7 +97,18 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account removeUserFromAccount(String userId, String accountId, String userIdToBeAssigned) throws UseException {
-        return null;
+        Account account = accountsRepository.getEntityById(accountId).orElseThrow(() -> new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_FOUND));
+        User otherUser = usersRepository.getEntityById(userIdToBeAssigned).orElseThrow();
+
+        if (!Objects.equals(userId, account.getOwner().getId())){
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_OWNER);
+        }
+        if (account.getUsers()
+                .noneMatch(x -> Objects.equals(x.getId(), userIdToBeAssigned))){
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.USER_NOT_ASSIGNED_TO_THIS_ACCOUNT);
+        }
+        account.removeUser(otherUser);
+        return accountsRepository.save(account);
     }
 
     @Override
@@ -100,19 +117,49 @@ public class AccountServiceImpl implements AccountService {
                 .orElseThrow(() -> new UseException(Activity.INACTIVATE_ACCOUNT, UseExceptionType.USER_NOT_FOUND));
         Account account = accountsRepository.getEntityById(accountId)
                 .orElseThrow(() -> new UseException(Activity.INACTIVATE_ACCOUNT, UseExceptionType.NOT_FOUND));
-        account.setActive(false);
 
         if (!Objects.equals(user.getId(), account.getOwner().getId())) {
             throw new UseException(Activity.INACTIVATE_ACCOUNT, UseExceptionType.NOT_OWNER);
-        } else if (!account.isActive() || !user.isActive()) {
+        }
+        if (!account.isActive() || !user.isActive()) {
             throw new UseException(Activity.INACTIVATE_ACCOUNT, UseExceptionType.NOT_ACTIVE);
         }
-
+        account.setActive(false);
         return accountsRepository.save(account);
     }
 
     @Override
     public Stream<Account> findAccounts(String searchValue, String userId, Integer pageNumber, Integer pageSize, SortOrder sortOrder) throws UseException {
-        return null;
+
+        if(!searchValue.equals("")){
+            return accountsRepository.all()
+                    .filter(x -> x.getName().toLowerCase().contains(searchValue));
+        } else if (sortOrder.toString().toLowerCase().equals("accountname") && pageSize == null){
+            return accountsRepository.all()
+                    .sorted(Comparator.comparing(Account::getName));
+        } else if (userId != null){
+            List<Account> usersAssociatedAccounts = new ArrayList<>();
+            List<Account> allAccountsInTheBank = accountsRepository.all().collect(Collectors.toList());
+
+            for(Account account : allAccountsInTheBank) {
+                if(Objects.equals(account.getOwner().getId(), userId)){
+                    usersAssociatedAccounts.add(account);
+                }
+                List<User> userList = account.getUsers().collect(Collectors.toList());
+                if (!userList.isEmpty()){
+                    for (User user : userList){
+                        if (Objects.equals(user.getId(), userId)){
+                            usersAssociatedAccounts.add(account);
+                        }
+                    }
+                }
+            }
+
+            return usersAssociatedAccounts.stream();
+
+        } else if(pageSize != null) {
+            return ListUtils.applyPage(accountsRepository.all().sorted(Comparator.comparing(Account::getName)), pageNumber, pageSize);
+        }
+        return accountsRepository.all();
     }
 }
